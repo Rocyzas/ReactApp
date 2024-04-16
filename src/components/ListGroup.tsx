70470703176a735ea803992f1feb8fd654567675
Rocyzas <pranas.rokas@gmail.com>
2024-04-16 22:06:45 +0100

    feat: on click added

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,22 @@
+import { MouseEvent } from "react";
 function ListGroup() {
+  const items = ["Kaunas", "Vilnius", "Siauliai", "Klaipeda"];
+
+  // event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item active" aria-current="true">
-        An active item
-      </li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>ListGroup</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
